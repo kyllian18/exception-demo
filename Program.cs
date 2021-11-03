ac2dabbd27b805285dda95c70f6f3f362e7a50e8
kyllian18 <64402572+kyllian18@users.noreply.github.com>
2021-11-03 17:18:11 +0000

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,86 @@
+﻿using System;
+
+namespace exceptiondemo
+{
+    class MainClass
+    {
+        public static void Main(string[] args)
+        {
+            // Index out of range
+            try
+            {
+                int[] numbers = new int[2];
+
+                numbers[0] = 0;
+                numbers[1] = 1;
+                numbers[2] = 2;
+
+                foreach (int i in numbers)
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            catch (IndexOutOfRangeException e)
+            {
+                Console.WriteLine($"IndexOutOfRangeException: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+            finally
+            {
+                Console.WriteLine("Finished process, exception thrown");
+            }
+
+            // Divide by zero
+            try
+            {
+
+            }
+            catch (DivideByZeroException e)
+            {
+                Console.WriteLine($"DivideByZeroException: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+            finally
+            {
+
+            }
+
+            // Argument exception
+            try
+            {
+                Show(null, 59);
+            }
+            catch (ArgumentException e)
+            {
+                Console.WriteLine($"ArgumentException: {e.Message}");
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine($"Exception: {e.Message}");
+            }
+            finally
+            { }
+
+        }
+
+        private static void Divide(int a, int b, string s)
+        {
+            int result = a / b;
+
+            Console.WriteLine($"{s.ToUpper()}: {result}");
+        }
+
+        private static void Show(string fname, int age)
+        {
+            if (fname == null)
+                throw new ArgumentException("Parameter cannot be null", nameof(fname);
+            Console.WriteLine($"{fname} is {age} years old");
+        }
+    }
+}
